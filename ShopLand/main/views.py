ae507ecf83f906a46076650772b6bdaa1f6b6d35
Maks Marcenukm <marcenukm995@gmail.com>
2025-02-18 00:16:53 +0200

    Add method in views

--- ShopLand/main/views.py
+++ ShopLand/main/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
+from .models import Category , Products
 # Create your views here.
+
+def popular_list(request):
+    products=Products.objects.filter(available=True)
+    return render(request,'main/index.html',{'products':products})
+
+def product_detail(request,slug):
+    products=get_object_or_404(Products,slug=slug,available=True)
+    return render (request,'main/detail.html',{'products':products})
+
