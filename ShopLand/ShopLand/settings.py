c3ef743053c42c25b706860d9ae6a670fb3e5d0a
Maks Marcenukm <marcenukm995@gmail.com>
2025-02-23 11:45:47 +0200

    add file for save key

--- ShopLand/ShopLand/settings.py
+++ ShopLand/ShopLand/settings.py
@@ -78,9 +78,13 @@ WSGI_APPLICATION = 'ShopLand.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+   'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'database_ShopLand',
+        'USER': 'postgres',
+        'PASSWORD': '123456',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
