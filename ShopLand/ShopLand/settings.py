777c26e489fad21193aee07533dc6475bc99474b
Maks Marcenukm <marcenukm995@gmail.com>
2025-02-23 11:56:25 +0200

    add file in gitignore

--- ShopLand/ShopLand/settings.py
+++ ShopLand/ShopLand/settings.py
@@ -11,6 +11,11 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import json
+
+
+#read file keyForProgram.json
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
