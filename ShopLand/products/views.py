d1b51e1134fb3685ffb372f721d56348e66a3fa2
Maks Marcenukm <marcenukm995@gmail.com>
2025-02-24 22:12:31 +0200

    added the code, now the categories are displayed in the modal window

--- ShopLand/products/views.py
+++ ShopLand/products/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
-
+from .models import  Category
 # Create your views here.
 def products_list(request):
-    return render(request,'products/home.html')
+    data_category=Category.objects.all()
+    context={
+        'data_category':data_category
+    }
+    return render(request,'products/home.html',context)
